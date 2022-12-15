87ad97407c4fb33141b6204397fb233429a0c0b2
DESKTOP-3QJ4BRH\LEGION <mostafaahmed10108@gmail.com>
2022-12-16 00:35:06 +0200

    V2

--- Libraries_Embedded_C/Bitmath.h
+++ Libraries_Embedded_C/Bitmath.h
@@ -8,39 +8,42 @@
 #ifndef BITMATH_H_
 #define BITMATH_H_
 
-#define SET_BIT(REG,Bit)			(REG)|=(1<<Bit)
 
-#define CLR_BIT(REG,Bit)			(REG)&=(0<<Bit)
+//Assuming an 8-bit register
 
-#define TGL_BIT(REG,Bit)			(REG)^=(1<<Bit)
+#define SET_BIT(REG,Bit)			        (REG)|=(1<<Bit)
+        
+#define CLR_BIT(REG,Bit)			        (REG)&=(0<<Bit)
+        
+#define TGL_BIT(REG,Bit)			        (REG)^=(1<<Bit)
 
-#define GET_BIT(REG,Bit)			REG&(1<<Bit)!=0
+#define GET_BIT(REG,Bit)			        REG&(1<<Bit)!=0
 
-#define SET_HIG_NIB(REG)			(REG)|=0xF0000000
+#define SET_HIG_NIB(REG)			       (REG)|=0xF0
 
-#define SET_LOW_NIB(REG)			(REG)|=0x0000000F
+#define SET_LOW_NIB(REG)			       (REG)|=0x0F
 
-#define CLR_HIG_NIB(REG)			(REG)&=0x0FFFFFFF
+#define CLR_HIG_NIB(REG)			       (REG)&=0x0FF
+  
+#define GET_HIG_NIB(REG)			       (REG)<<4 & 0xF0
 
-#define GET_HIG_NIB(REG)			(REG)<<4 & 0xF0
+#define TGL_HIG_NIB(REG)              REG ^= 0xF0
 
-#define TGL_HIG_NIB(REG)
+#define ASSIGN_REG(REG,Value)		     (REG) |= (Value)
 
-#define ASSIGN_REG(REG,Value)		(REG) |= (Value)
+#define ASSIGN_HIG_NIB(REG,Value)     (REG) = (Value)
 
-#define ASSIGN_HIG_NIB(REG,Value)
+#define GET_HIG_NIB(REG)			       (REG)&(0xF000000)
 
-#define GET_HIG_NIB(REG)			(REG)&(0xF000000)
+#define GET_LOW_NIB(REG)			       (REG)&(0x000000F)
 
-#define GET_LOW_NIB(REG)			(REG)&(0x000000F)
+#define SHIFT_RIGHT(REG,Value)		    (REG)>>(Value)
 
-#define SHIFT_RIGHT(REG,Value)		(REG)>>(Value)
+#define SHIFT_LEFT(REG,Value)	        (REG)<<(Value)
 
-#define SHIFT_LEFT(REG,Value)		(REG)<<(Value)
+#define CIR_SHIFT_RIGHT(REG,Value)    REG = ((REG<<Value)|(REG>>(sizeof(REG)-Value)))
 
-#define CIR_SHIFT_RIGHT(REG,Value)  REG = ((REG<<Value)|(REG>>(sizeof(REG)-Value)))
-
-#define CIR_SHIFT_LEFT(REG,Bit)		REG = ((REG>>Value)|(REG<<(sizeof(REG)-Value)))
+#define CIR_SHIFT_LEFT(REG,Bit)		    REG = ((REG>>Value)|(REG<<(sizeof(REG)-Value)))
 
 
 
